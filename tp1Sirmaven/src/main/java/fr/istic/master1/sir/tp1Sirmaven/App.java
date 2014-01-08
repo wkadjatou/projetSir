2c838e1404bdc08211b6a5792ddff254420368df
wkadjatou <missbaldez1@yahoo.fr>
2014-01-08 17:59:23 +0100

    init commit

---
+++ tp1Sirmaven/src/main/java/fr/istic/master1/sir/tp1Sirmaven/App.java
@@ -0,0 +1,13 @@
+package fr.istic.master1.sir.tp1Sirmaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
